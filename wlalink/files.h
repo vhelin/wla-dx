31b60d5acbe35d25d06983474c93d310de3f2130
galaxyhaxz <andrew-haxz@hotmail.com>
2016-09-22 13:15:32 -0500

    Update to remove linkfile

--- wlalink/files.h
+++ wlalink/files.h
@@ -2,8 +2,9 @@
 #ifndef _FILES_H
 #define _FILES_H
 
-int load_files(char *argv[], int argc);
-int load_file(char *fn, int bank, int slot, int base, int base_defined);
+int load_objects(char *argv[], int argc);
+int load_library(char *argv, int contains_flag);
+int load_file(char *fn);
 int load_file_data(char *fn, unsigned char **da, int *size);
 char *get_file_name(int id);
 char *get_source_file_name(int file_id, int source_id);
