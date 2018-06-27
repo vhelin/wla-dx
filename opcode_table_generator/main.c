a716095b6e8df1c27c9ec51f29e27ad9668f4cc8
Neui <neuisen@googlemail.com>
2018-06-27 13:14:42 +0200

    opgen: Print out the error when it couldn't open the file

--- opcode_table_generator/main.c
+++ opcode_table_generator/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #if defined(AMIGA)
 #include "/defines.h"
@@ -100,7 +101,7 @@ int main(int argc, char *argv[]) {
   if (outname) {
     out = fopen(outname, "wb");
     if (out == NULL) {
-      fprintf(stderr, "MAIN: Unable to create file \"%s\".\n", outname);
+      fprintf(stderr, "MAIN: Unable to open file \"%s\": %s\n", outname, strerror(errno));
       return 1;
     }
   }
