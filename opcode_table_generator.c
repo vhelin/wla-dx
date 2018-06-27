28d0e7946d919444d7626d58e833e7924c947c0a
Neui <neuisen@googlemail.com>
2018-06-27 13:14:42 +0200

    opgen: Print out the error when it couldn't open the file

--- opcode_table_generator.c
+++ opcode_table_generator.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "defines.h"
 
@@ -94,7 +95,7 @@ int main(int argc, char *argv[]) {
   if (outname) {
     out = fopen(outname, "wb");
     if (out == NULL) {
-      fprintf(stderr, "MAIN: Unable to create file \"%s\".\n", outname);
+      fprintf(stderr, "MAIN: Unable to open file \"%s\": %s\n", outname, strerror(errno));
       return 1;
     }
   }
