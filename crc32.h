173bf7697697ebba0620fe619a0b29b4fc7c35b4
David Crooks <davidacrooks@outlook.com>
2018-07-25 15:22:09 -0400

    Moving crc32 functions from hashmap to separate crc32 file

---
+++ crc32.h
@@ -0,0 +1,10 @@
+/*
+* Generic crc32 helper function
+*/
+
+#ifndef __CRC32_H__
+#define __CRC32_H__
+
+extern unsigned long crc32(const unsigned char *s, unsigned int len);
+
+#endif // __CRC32_H__
