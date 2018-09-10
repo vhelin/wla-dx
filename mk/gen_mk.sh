17e4c9528dc27b98bdb23bf07bc4905777096211
Neui <neuisen@googlemail.com>
2018-09-10 16:44:39 +0200

    mk: Add legacy makefile generator
    "legacy" as in it doesn't use CMake. It simply generates makefiles
    out of a configuration in `generate.sh`.

---
+++ mk/gen_mk.sh
@@ -0,0 +1,170 @@
+#!/bin/sh
+
+# This script generates an Makefile and outputs it to stdout.
+# Use generate.sh to generate preconfigured makefiles instread!
+#
+# Accepted parameters are passed through environment variables:
+# RM - Remove command (default: rm -rf)
+# CC - C compiler (default: $CC, otherwise make default)
+# COMPILE_DEF - Format string for defining defs (default: -D%s)
+# CFLAGS - C flags, overwritable by user (default: make default)
+# CFLAGS_MISC - Misc C flags (default: )
+# CFLAGS_OPT - C flags about optimization (default: -O3)
+# LD - Linker (default: $LD, or \$(CC))
+# LDFLAGS - Linker flags, overwritable by user (default: make default)
+# LDFLAGS_MISC - Misc linker flags (default: )
+# LDLIBS - Libraries to link with (std c and math lib) (default: -l c -l m)
+# LDLIBS_GEN - Libraries to link when making gen-binaries (default: -l c)
+#
+# Uses echo, printf, tr, sed, awk, cat and sh
+#env >&2
+
+# WLA name, DEFINE name
+TARGETS="
+gb,GB
+65c02,WDC65C02
+6502,MCS6502
+6510,MCS6510
+65816,W65816
+huc6280,HUC6280
+spc700,SPC700
+z80,Z80
+"
+# Processed Targets, first column is the "object number"
+PTARGETS="$(echo "$TARGETS" | awk -F , 'NF {print NR-1 "," $0}')"
+
+WLA_SRCS="
+main.c
+hashmap.c
+parse.c
+include_file.c
+pass_1.c
+pass_2.c
+pass_3.c
+pass_4.c
+stack.c
+listfile.c
+crc32.c
+"
+WLA_SRCS="$(echo "$WLA_SRCS" | tr '\n' ' ')"
+
+WLALINK_SRCS="
+wlalink/main.c
+wlalink/memory.c
+wlalink/parse.c
+wlalink/files.c
+wlalink/check.c
+wlalink/analyze.c
+wlalink/write.c
+wlalink/compute.c
+wlalink/discard.c
+wlalink/listfile.c
+hashmap.c
+crc32.c
+"
+WLALINK_SRCS="$(echo "$WLALINK_SRCS" | tr '\n' ' ')"
+
+WLAB_SRCS="
+wlab/main.c
+"
+WLAB_SRCS="$(echo "$WLAB_SRCS" | tr '\n' ' ')"
+
+# \1 is the first part, \2 is the second, etc.
+c() {
+    sed -e 's#\([[:alnum:]]\+\),\([[:alnum:]]\+\)\+,\([[:alnum:]]\+\)\+#'"$1"'#g' | tr '\n' ' '
+}
+cc() {
+    echo "$PTARGETS" | c "$*"
+}
+pf() {
+    printf -- "$1" "$2"
+}
+
+# Header
+RM="${RM:-rm -rf}"
+CC="${CC}"
+COMPILE_DEF="${COMPILE_DEF:--D%s}"
+CFLAGS_OPT="${CFLAGS_OPT:--O3}"
+DBGFLAGS="$(pf "$COMPILE_DEF" "NDEBUG=1")"
+LD="${LD:-\$(CC)}"
+LDLIBS="${LDLIBS:--l c -l m}"
+LDLIBS_GEN="${LDLIBS:--l c}"
+
+cat << EOF
+#!/usr/bin/env make -f
+# This makefile has been auto-generated.
+.POSIX:
+
+WLA_SRCS=${WLA_SRCS}
+WLALINK_SRCS=${WLALINK_SRCS}
+WLAB_SRCS=${WLAB_SRCS}
+RM=${RM}
+$(test -z "$CC" || echo "CC=${CC}")
+CFLAGS_ALL=${CFLAGS_MISC} ${CFLAGS_OPT} \$(CFLAGS)
+DEBUGFLAGS=${DBGFLAGS}
+LD=${LD}
+LDFLAGS_ALL=${LDFLAGS_MISC} \$(LDFLAGS)
+LDLIBS=${LDLIBS}
+LDLIBS_GEN=${LDLIBS_GEN}
+
+.c.o:
+	\$(CC) \$(DEBUGFLAGS) \$(CFLAGS_ALL) -c \$*.c -o \$@
+
+all: wlab wlalink $(cc 'wla-\2')
+generators: $(cc 'gen-\2')
+
+clean: clean-wlab clean-wlalink clean-wla clean-gen
+clean-objects:
+	-\$(RM) $(cc '$(WLA_SRCS:.c=.o\1) $(WLA_\2_GENO) $(WLA_\2_GENSO)')
+	-\$(RM) \$(WLAB_SRCS:.c=.o) \$(WLALINK_SRCS:.c=.o)
+clean-wlab:
+	-\$(RM) binaries/wlab
+	-\$(RM) \$(WLAB_SRCS:.c=.o)
+clean-wlalink:
+	-\$(RM) binaries/wlalink
+	-\$(RM) \$(WLALINK_SRCS:.c=.o)
+clean-wla: $(cc 'clean-wla-\2')
+clean-gen: $(cc 'clean-gen-\2')
+
+wlab: binaries/wlab
+binaries/wlab: \$(WLAB_SRCS:.c=.o)
+	\$(LD) \$(LDFLAGS_ALL) \$(WLAB_SRCS:.c=.o) \$(LDLIBS) \$@
+
+wlalink: binaries/wlalink
+binaries/wlalink: \$(WLALINK_SRCS:.c=.o)
+	\$(LD) \$(LDFLAGS_ALL) \$(WLALINK_SRCS:.c=.o) \$(LDLIBS) \$@
+EOF
+
+# Now let's generate stuff for each cpu/arch supported
+WO=0 # Object number, so the object file extension is <=3 chars
+for TARGET in $PTARGETS; do
+    W="$(printf "$TARGET" | c '\2')"
+    WO=$((WO+1))
+    OW="o$WO" # Object file extension
+    D="$(printf "$TARGET" | c '\3')"
+    cat << EOF
+
+WLA_${W}_O=\$(WLA_SRCS:.c=.${OW})
+WLA_${W}_GEN=opcode_table_generator.c opcodes_${W}.c
+WLA_${W}_GENO=\$(WLA_${W}_GEN:.c=.${OW})
+WLA_${W}_GENS=opcodes_${W}_tables.c opcodes_${W}.c
+WLA_${W}_GENSO=\$(WLA_${W}_GENS:.c=.${OW})
+WLA_${W}_FLAGS=$(printf -- "$COMPILE_DEF" "${D}=1")
+.SUFFIXES: .${OW}
+.c.${OW}:
+	\$(CC) \$(DEBUGFLAGS) \$(WLA_${W}_FLAGS) \$(CFLAGS_ALL) -c \$*.c -o \$@
+wla-${W}: binaries/wla-${W}
+binaries/wla-${W}: \$(WLA_${W}_O) \$(WLA_${W}_GENSO)
+	\$(LD) \$(LDFLAGS_ALL) \$(WLA_${W}_O) \$(WLA_${W}_GENSO) \$(LDLIBS) \$@
+gen-${W}: \$(WLA_${W}_GENO)
+	\$(LD) \$(LDFLAGS_ALL) \$(WLA_${W}_GENO) \$(LDLIBS_GEN) \$@
+opcodes_${W}_tables.c: gen-${W}
+	./\$< \$@
+clean-wla-${W}:
+	-\$(RM) binaries/wla-${W} \$(WLA_${W}_O)
+clean-gen-${W}:
+	-\$(RM) gen-${W} \$(WLA_${W}_GENO) \$(WLA_${W}_GENSO)
+EOF
+    done
+
+
---
+++ mk/generate.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+OUTDIR=mk
+test ! -d mk && OUTDIR=.
+
+# Calls the actual generator
+gen(){
+    (
+    export RM
+    export CC COMPILE_DEF CFLAGS CFLAGS_MISC CFLAGS_OPT
+    export LD LDFLAGS LDFLAGS_MISC LDLIBS LDLIBS_GEN
+    test -f mk/gen_mk.sh && ./mk/gen_mk.sh \
+            || ( test -f ./gen_mk.sh && ./gen_mk.sh )
+    )
+}
+
+SETUPS=posix
+setup_posix() {
+    # See gen_mk.sh for more information about the variables
+    # This is meant to set everything to their default values,
+    # so just keep it empty
+    RM=
+    CC=
+    COMPILE_DEF=
+    CFLAGS=
+    CFLAGS_MISC=
+    CFLAGS_OPT=
+    LD=
+    LDFLAGS=
+    LDFLAGS_MISC=
+    LDLIBS=
+    LDLIBS_GEN=
+    test -z "$1" || "$*"
+}
+
+generate_for() {
+    for target in $SETUPS; do
+        test "$1" = "$target" && setup_"$target" gen > "$OUTDIR"/"$target".mk
+    done
+}
+
+IFS=' '
+if [ -z "$1" ]; then
+    cat << EOF
+Usage: $0 all
+       $0 <target>
+Use 'all' to generate an makefile for every target this script can
+generate for.
+Use '<target>' to only generate specific targets. Available targets:
+EOF
+    for target in $SETUPS; do
+        echo "    $target"
+    done
+    exit 1
+elif [ "$1" = "all" ]; then
+    for target in $SETUPS; do
+        generate_for "$target"
+    done
+else
+    while ! [ -z "$1" ]; do
+        generate_for "$1"
+        shift
+    done
+fi
+
