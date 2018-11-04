39b54ff47fa4d42c61f3b8707dbd4296f3909e7c
Neui <neuisen@googlemail.com>
2018-11-04 20:46:19 +0100

    65816/linker_test: Generate library

--- tests/65816/linker_test/test.cmake
+++ tests/65816/linker_test/test.cmake
@@ -1,1 +1,8 @@
-set(DEFS "NAME=kullervo")
+list(APPEND DEFS "NAME=kullervo")
+
+wla(LIBRARY
+    ARCH "${ARCH}"
+    SOURCE "${SOURCE_DIR}/test.s"
+    OUTPUT "${BINARY_DIR}/test.lib"
+    VERBOSE)
+
