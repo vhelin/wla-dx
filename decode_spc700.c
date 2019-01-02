6a5049e7fa743a24e8b1ebc9f529e0d51ba7876b
hal <verlvcis@gmail.com>
2019-01-03 00:59:29 +0900

    Add a new decode type for NOT1 and bit operations with carry flag

--- decode_spc700.c
+++ decode_spc700.c
@@ -74,7 +74,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	  output_assembled_opcode(opt_tmp, "k%d d%d r%s ", active_file_info_last->line_current, opt_tmp->hex, label);
 	else
 	  output_assembled_opcode(opt_tmp, "d%d C%d ", opt_tmp->hex, latest_stack);
-	
+
 	i = inz;
 	return SUCCEEDED;
       }
@@ -119,18 +119,18 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	  break;
 	if (d > 7 || d < 0)
 	  break;
-	
+
 	for (x++; x < OP_SIZE_MAX; inz++, x++) {
 	  if (opt_tmp->op[x] == 0 && buffer[inz] == 0x0A) {
 	    output_assembled_opcode(opt_tmp, "d%d ", opt_tmp->hex | (d << 5));
-	    
+
 	    if (v == SUCCEEDED)
 	      output_assembled_opcode(opt_tmp, "d%d ", e);
 	    else if (v == INPUT_NUMBER_ADDRESS_LABEL)
 	      output_assembled_opcode(opt_tmp, "k%d R%s ", active_file_info_last->line_current, labelx);
 	    else
 	      output_assembled_opcode(opt_tmp, "c%d ", h);
-	    
+
 	    i = inz;
 	    return SUCCEEDED;
 	  }
@@ -179,18 +179,18 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	  break;
 	if (d > 7 || d < 0)
 	  break;
-	
+
 	for (x++; x < OP_SIZE_MAX; inz++, x++) {
 	  if (opt_tmp->op[x] == 0 && buffer[inz] == 0x0A) {
 	    output_assembled_opcode(opt_tmp, "d%d ", opt_tmp->hex | (((d << 1) + 1) << 4));
-	    
+
 	    if (v == SUCCEEDED)
 	      output_assembled_opcode(opt_tmp, "d%d ", e);
 	    else if (v == INPUT_NUMBER_ADDRESS_LABEL)
 	      output_assembled_opcode(opt_tmp, "k%d R%s ", active_file_info_last->line_current, labelx);
 	    else
 	      output_assembled_opcode(opt_tmp, "c%d ", h);
-	    
+
 	    i = inz;
 	    return SUCCEEDED;
 	  }
@@ -284,7 +284,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	      /* let's configure the stack so that all label references inside are relative */
 	      stacks_tmp->relative_references = 1;
 	    }
-	    
+
 	    i = inz;
 	    return SUCCEEDED;
 	  }
@@ -342,14 +342,14 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	      output_assembled_opcode(opt_tmp, "k%d d%d R%s ", active_file_info_last->line_current, opt_tmp->hex, label);
 	    else
 	      output_assembled_opcode(opt_tmp, "d%d c%d ", opt_tmp->hex, latest_stack);
-	    
+
 	    if (v == SUCCEEDED)
 	      output_assembled_opcode(opt_tmp, "d%d ", e);
 	    else if (v == INPUT_NUMBER_ADDRESS_LABEL)
 	      output_assembled_opcode(opt_tmp, "k%d R%s ", active_file_info_last->line_current, labelx);
 	    else
 	      output_assembled_opcode(opt_tmp, "c%d ", h);
-	    
+
 	    i = inz;
 	    return SUCCEEDED;
 	  }
@@ -386,7 +386,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
     h = latest_stack;
     if (z == INPUT_NUMBER_ADDRESS_LABEL)
       strcpy(labelx, label);
-    
+
     for (x++; x < OP_SIZE_MAX; inz++, x++) {
       if (opt_tmp->op[x] == '~') {
 	y = i;
@@ -399,7 +399,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	if (d > 7 || d < 0)
 	  break;
 	g = d;
-	
+
 	for (x++; x < OP_SIZE_MAX; inz++, x++) {
 	  if (opt_tmp->op[x] == 'x') {
 	    y = i;
@@ -415,7 +415,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	    for (x++; x < OP_SIZE_MAX; inz++, x++) {
 	      if (opt_tmp->op[x] == 0 && buffer[inz] == 0x0A) {
 		output_assembled_opcode(opt_tmp, "d%d ", opt_tmp->hex | (g << 5));
-		
+
 		if (v == SUCCEEDED)
 		  output_assembled_opcode(opt_tmp, "d%d ", e);
 		else if (v == INPUT_NUMBER_ADDRESS_LABEL)
@@ -432,7 +432,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 		  /* let's configure the stack so that all label references inside are relative */
 		  stacks_tmp->relative_references = 1;
 		}
-		
+
 		i = inz;
 		return SUCCEEDED;
 	      }
@@ -473,7 +473,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
     h = latest_stack;
     if (z == INPUT_NUMBER_ADDRESS_LABEL)
       strcpy(labelx, label);
-    
+
     for (x++; x < OP_SIZE_MAX; inz++, x++) {
       if (opt_tmp->op[x] == '~') {
 	y = i;
@@ -502,14 +502,14 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	    for (x++; x < OP_SIZE_MAX; inz++, x++) {
 	      if (opt_tmp->op[x] == 0 && buffer[inz] == 0x0A) {
 		output_assembled_opcode(opt_tmp, "d%d ", opt_tmp->hex | (((g << 1) + 1) << 4));
-		
+
 		if (v == SUCCEEDED)
 		  output_assembled_opcode(opt_tmp, "d%d ", e);
 		else if (v == INPUT_NUMBER_ADDRESS_LABEL)
 		  output_assembled_opcode(opt_tmp, "k%d R%s ", active_file_info_last->line_current, labelx);
 		else
 		  output_assembled_opcode(opt_tmp, "c%d ", h);
-		
+
 		if (z == SUCCEEDED)
 		  output_assembled_opcode(opt_tmp, "d%d ", d);
 		else if (z == INPUT_NUMBER_ADDRESS_LABEL)
@@ -519,7 +519,7 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 		  /* let's configure the stack so that all label references inside are relative */
 		  stacks_tmp->relative_references = 1;
 		}
-		
+
 		i = inz;
 		return SUCCEEDED;
 	      }
@@ -531,6 +531,64 @@ for ( ; x < OP_SIZE_MAX; inz++, x++) {
 	    break;
 	}
       }
+      if (opt_tmp->op[x] != toupper((int)buffer[inz]))
+	break;
+    }
+  }
+  if (opt_tmp->op[x] != toupper((int)buffer[inz]))
+    break;
+ }
+break;
+
+case 0xF:
+for ( ; x < OP_SIZE_MAX; inz++, x++) {
+  if (opt_tmp->op[x] == '?') {
+    y = i;
+    i = inz;
+    parse_floats = NO;
+    z = input_number();
+    parse_floats = YES;
+    inz = i;
+    i = y;
+    if (!(z == SUCCEEDED || z == INPUT_NUMBER_ADDRESS_LABEL || z == INPUT_NUMBER_STACK))
+      return FAILED;
+    if (z == SUCCEEDED && (d > 8191 || d < 0))
+      break;
+
+    e = d;
+    v = z;
+    h = latest_stack;
+    if (z == INPUT_NUMBER_ADDRESS_LABEL)
+      strcpy(labelx, label);
+
+    for (x++; x < OP_SIZE_MAX; inz++, x++) {
+      if (opt_tmp->op[x] == '~') {
+	y = i;
+	i = inz;
+	z = input_number();
+	inz = i;
+	i = y;
+	if (z != SUCCEEDED)
+	  break;
+	if (d > 7 || d < 0)
+	  break;
+
+	for (x++; x < OP_SIZE_MAX; inz++, x++) {
+	  if (opt_tmp->op[x] == 0 && buffer[inz] == 0x0A) {
+	    if (v == SUCCEEDED)
+	      output_assembled_opcode(opt_tmp, "d%d y%d ", opt_tmp->hex, e | d << 13);
+	    else if (v == INPUT_NUMBER_ADDRESS_LABEL)
+	      output_assembled_opcode(opt_tmp, "k%d d%d r%s ", active_file_info_last->line_current, opt_tmp->hex, labelx);
+	    else
+	      output_assembled_opcode(opt_tmp, "d%d C%d ", opt_tmp->hex, h);
+
+	    i = inz;
+	    return SUCCEEDED;
+	  }
+	  if (opt_tmp->op[x] != toupper((int)buffer[inz]))
+	    break;
+	}
+       }
       if (opt_tmp->op[x] != toupper((int)buffer[inz]))
 	break;
     }
