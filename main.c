5afdbd24c8979e175710d0a69dd3599b8939d69d
Ville Helin <ville.helin@gmail.com>
2019-08-15 21:17:42 +0300

    Started to work on v9.10...

--- main.c
+++ main.c
@@ -41,43 +41,43 @@ long __stack = 200000;
 #endif
 
 #ifdef GB
-char version_string[] = "$VER: WLA-GB 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-GB 9.10a (15.8.2019)";
 #endif
 #ifdef Z80
-char version_string[] = "$VER: WLA-Z80 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-Z80 9.10a (15.8.2019)";
 #endif
 #ifdef MCS6502
-char version_string[] = "$VER: WLA-6502 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-6502 9.10a (15.8.2019)";
 #endif
 #ifdef WDC65C02
-char version_string[] = "$VER: WLA-65C02 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-65C02 9.10a (15.8.2019)";
 #endif
 #ifdef W65816
-char version_string[] = "$VER: WLA-65816 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-65816 9.10a (15.8.2019)";
 #endif
 #ifdef MCS6510
-char version_string[] = "$VER: WLA-6510 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-6510 9.10a (15.8.2019)";
 #endif
 #ifdef MC6800
-char version_string[] = "$VER: WLA-6800 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-6800 9.10a (15.8.2019)";
 #endif
 #ifdef MC6801
-char version_string[] = "$VER: WLA-6801 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-6801 9.10a (15.8.2019)";
 #endif
 #ifdef MC6809
-char version_string[] = "$VER: WLA-6809 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-6809 9.10a (15.8.2019)";
 #endif
 #ifdef SPC700
-char version_string[] = "$VER: WLA-SPC700 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-SPC700 9.10a (15.8.2019)";
 #endif
 #ifdef HUC6280
-char version_string[] = "$VER: WLA-HuC6280 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-HuC6280 9.10a (15.8.2019)";
 #endif
 #ifdef I8008
-char version_string[] = "$VER: WLA-8008 9.9 (15.8.2019)";
+char version_string[] = "$VER: WLA-8008 9.10a (15.8.2019)";
 #endif
 
-char wla_version[] = "9.9";
+char wla_version[] = "9.10a";
 
 char *tmp_name = NULL;
 
@@ -162,40 +162,40 @@ int main(int argc, char *argv[]) {
   
   if (output_format == OUTPUT_NONE || parse_flags_result == FAILED) {
 #ifdef GB
-    printf("\nWLA GB-Z80 Macro Assembler v9.9\n");
+    printf("\nWLA GB-Z80 Macro Assembler v9.10a\n");
 #endif
 #ifdef Z80
-    printf("\nWLA Z80 Macro Assembler v9.9\n");
+    printf("\nWLA Z80 Macro Assembler v9.10a\n");
 #endif
 #ifdef MCS6502
-    printf("\nWLA 6502 Macro Assembler v9.9\n");
+    printf("\nWLA 6502 Macro Assembler v9.10a\n");
 #endif
 #ifdef WDC65C02
-    printf("\nWLA 65C02 Macro Assembler v9.9\n");
+    printf("\nWLA 65C02 Macro Assembler v9.10a\n");
 #endif
 #ifdef MC6800
-    printf("\nWLA 6800 Macro Assembler v9.9\n");
+    printf("\nWLA 6800 Macro Assembler v9.10a\n");
 #endif
 #ifdef MC6801
-    printf("\nWLA 6801 Macro Assembler v9.9\n");
+    printf("\nWLA 6801 Macro Assembler v9.10a\n");
 #endif
 #ifdef MC6809
-    printf("\nWLA 6809 Macro Assembler v9.9\n");
+    printf("\nWLA 6809 Macro Assembler v9.10a\n");
 #endif
 #ifdef I8008
-    printf("\nWLA 8008 Macro Assembler v9.9\n");
+    printf("\nWLA 8008 Macro Assembler v9.10a\n");
 #endif
 #ifdef W65816
-    printf("\nWLA 65816 Macro Assembler v9.9\n");
+    printf("\nWLA 65816 Macro Assembler v9.10a\n");
 #endif
 #ifdef MCS6510
-    printf("\nWLA 6510 Macro Assembler v9.9\n");
+    printf("\nWLA 6510 Macro Assembler v9.10a\n");
 #endif
 #ifdef SPC700
-    printf("\nWLA SPC-700 Macro Assembler v9.9\n");
+    printf("\nWLA SPC-700 Macro Assembler v9.10a\n");
 #endif
 #ifdef HUC6280
-    printf("\nWLA HuC6280 Macro Assembler v9.9\n");
+    printf("\nWLA HuC6280 Macro Assembler v9.10a\n");
 #endif
     printf("Written by Ville Helin in 1998-2008 - In GitHub since 2014: https://github.com/vhelin/wla-dx\n");
 #ifdef WLA_DEBUG
