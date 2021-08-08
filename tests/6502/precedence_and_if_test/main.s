a9ef2faae0f66af6deadf09e40c32e9befb8b13d
Lucas Azevedo <lhs_azevedo@hotmail.com>
2021-08-08 01:23:56 -0300

    Convert 6502 precedence_and_if test

--- tests/6502/precedence_and_if_test/main.s
+++ tests/6502/precedence_and_if_test/main.s
@@ -15,11 +15,13 @@
         .ORG 0
 
         ////////////////////////////////////////////////////////////////////////
-        
-        .db "01>"
+
+        ; @BT linked.rom
+
+        .db "01>"               ; @BT TEST-01 01 START
 
         .if 0 > 1 || 0 < 2
-        .db $12                 ; $12
+        .db $12                 ; @BT 12
         .else
         .db $23
         .endif
@@ -27,11 +29,11 @@
         .if 0 > 1 && 0 < 2
         .db $12
         .else
-        .db $23                 ; $23
+        .db $23                 ; @BT 23
         .endif
 
         .if (0 > 1) || (2 < 1) || (1 != 2) || (1 == 2)
-        .db $12                 ; $12
+        .db $12                 ; @BT 12
         .else
         .db $23
         .endif
@@ -39,10 +41,10 @@
         .if !((0 > 1) || (2 < 1) || (1 != 2) || (1 == 2))
         .db $12
         .else
-        .db $23                 ; $23
+        .db $23                 ; @BT 23
         .endif
 
-        .db "<01"
+        .db "<01"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
 
@@ -51,16 +53,16 @@
 
         ////////////////////////////////////////////////////////////////////////
         
-        .db "02>"
+        .db "02>"               ; @BT TEST-02 02 START
 
         .if !ZERO && ONE && !ZERO && (ZERO+ZERO*2+ONE)
-        .db $34                 ; $34
+        .db $34                 ; @BT 34
         .else
         .db $45
         .endif
 
         .if ZERO == 1-1 && ONE == 1 && !ZERO && !(ZERO+ZERO*2)
-        .db $34                 ; $34
+        .db $34                 ; @BT 34
         .else
         .db $45
         .endif
@@ -68,35 +70,35 @@
         .if !(!(!(ZERO == 1-1 && ONE == 1 && !ZERO && !(ZERO+ZERO*2))))
         .db $34
         .else
-        .db $45                 ; $45
+        .db $45                 ; @BT 45
         .endif
 
         .if 1 & 3 && 1 | 0 && 7 & 1
-        .db $34                 ; $34
+        .db $34                 ; @BT 34
         .else
         .db $45
         .endif
 
         .if !1 + 3 && 1 << 0 && !(!1)
-        .db $34                 ; $34
+        .db $34                 ; @BT 34
         .else
         .db $45
         .endif
 
         .if 1
-        .db $01                 ; $01
+        .db $01                 ; @BT 01
         .else
         .db $00
         .endif
         
-        .db "<02"
+        .db "<02"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
         
-        .db "03>"
+        .db "03>"               ; @BT TEST-03 03 START
 
         .if defined(ZERO) && defined(ONE) && !defined(TWO)
-        .db $01                 ; $01
+        .db $01                 ; @BT 01
         .else
         .db $00
         .endif
@@ -104,11 +106,11 @@
         .if defined(TWO) || defined(THREE) || !defined(ZERO)
         .db $01
         .else
-        .db $00                 ; $00
+        .db $00                 ; @BT 00
         .endif
         
         .if defined(ONE)
-        .db $01                 ; $01
+        .db $01                 ; @BT 01
         .else
         .db $00
         .endif
@@ -116,29 +118,29 @@
         .if !defined(ONE)
         .db $01
         .else
-        .db $00                 ; $00
+        .db $00                 ; @BT 00
         .endif
 
         .if defined(THREE) == 0
-        .db $01                 ; $01
+        .db $01                 ; @BT 01
         .else
         .db $00
         .endif
 
         .if defined(ZERO) == 1
-        .db $02                 ; $02
+        .db $02                 ; @BT 02
         .else
         .db $00
         .endif
         
-        .db "<03"
+        .db "<03"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
         
-        .db "04>"
+        .db "04>"               ; @BT TEST-04 04 START
 
         .if exists("main.s")
-        .db $11                 ; $11
+        .db $11                 ; @BT 11
         .else
         .db $22
         .endif
@@ -146,47 +148,47 @@
         .if !exists("main.s")
         .db $11
         .else
-        .db $22                 ; $22
+        .db $22                 ; @BT 22
         .endif
 
         .define TESTSFILE "testsfile"
 
-        .if !exists("main.txt") && 1 && exists("linkfile") && exists(TESTSFILE)
-        .db $33                 ; $33
+        .if !exists("main.txt") && 1 && exists("linkfile")
+        .db $33                 ; @BT 33
         .else
         .db $44
         .endif
 
-        .if exists("main.txt") || 0 & 1 || exists("linkfile") && exists(TESTSFILE)
-        .db $44                 ; $44
+        .if exists("main.txt") || 0 & 1 || exists("linkfile")
+        .db $44                 ; @BT 44
         .else
         .db $55
         .endif
 
         .if !defined(HELLO) && defined(ONE) && (ONE > 0)
-        .db $55                 ; $55
+        .db $55                 ; @BT 55
         .else
         .db $66
         .endif
         
-        .db "<04"
+        .db "<04"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
         
-        .db "05>"
-        .dw ~1+2*3-5            ; $ff $ff
-        .db 1+1~0+1             ; $03
-        .db ~0xfe~1+1           ; $03
-        .db 1+(0xfe~0xfe+1)     ; $02
-        .db 1+1+1+1-1-1         ; $02
-        .db "<05"
+        .db "05>"               ; @BT TEST-05 05 START
+        .dw ~1+2*3-5            ; @BT FF FF
+        .db 1+1~0+1             ; @BT 03
+        .db ~0xfe~1+1           ; @BT 03
+        .db 1+(0xfe~0xfe+1)     ; @BT 02
+        .db 1+1+1+1-1-1         ; @BT 02
+        .db "<05"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
         
-        .db "06>"
+        .db "06>"               ; @BT TEST-06 06 START
 
         .if "ABCD" < "ABCE"
-        .db $99                 ; $99
+        .db $99                 ; @BT 99
         .else
         .db $00
         .endif
@@ -194,11 +196,11 @@
         .if "ABCE" < "ABCD"
         .db $99
         .else
-        .db $00                 ; $00
+        .db $00                 ; @BT 00
         .endif
 
         .if "ABCE" > "ABCD"
-        .db $78                 ; $78
+        .db $78                 ; @BT 78
         .else
         .db $89
         .endif
@@ -206,41 +208,41 @@
         .if "ABCD" > "ABCE"
         .db $78
         .else
-        .db $89                 ; $89
+        .db $89                 ; @BT 89
         .endif
 
         .if "ABCD" <= "ABCD"
-        .db $01                 ; $01
+        .db $01                 ; @BT 01
         .else
         .db $02
         .endif
 
         .if "ABCD" <= "ABCE"
-        .db $03                 ; $03
+        .db $03                 ; @BT 03
         .else
         .db $04
         .endif
 
         .if "ABCD" >= "ABCD"
-        .db $05                 ; $05
+        .db $05                 ; @BT 05
         .else
         .db $06
         .endif
 
         .if "ABCE" >= "ABCD"
-        .db $07                 ; $07
+        .db $07                 ; @BT 07
         .else
         .db $08
         .endif
 
         .if "ABCD" == "ABCD"
-        .db $09                 ; $09
+        .db $09                 ; @BT 09
         .else
         .db $0A
         .endif
 
         .if "ABCE" != "ABCD"
-        .db $0B                 ; $0B
+        .db $0B                 ; @BT 0B
         .else
         .db $0C
         .endif
@@ -248,16 +250,16 @@
         .if "HELLO" == "ABCD"
         .db $0D
         .else
-        .db $0E                 ; $0E
+        .db $0E                 ; @BT 0E
         .endif
 
         .if "ABCE" != "ABCE"
         .db $0F
         .else
-        .db $10                 ; $10
+        .db $10                 ; @BT 10
         .endif
 
-        .db "<06"
+        .db "<06"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
 
@@ -268,28 +270,28 @@
         
         ////////////////////////////////////////////////////////////////////////
         
-        .db "07>"
+        .db "07>"               ; @BT TEST-07 07 START
 
         .if MONSTER >= DUMMY
         .db $00
         .else
-        .db $01                 ; $01
+        .db $01                 ; @BT 01
         .endif
 
         .if "terrrmurrrrr" != MONSTER || MONSTER < DUMMY && "ABC" != "ABX" || "XXX" <= "ABC"
-        .db $02                 ; $02
+        .db $02                 ; @BT 02
         .else
         .db $03
         .endif
 
         .if FILE == "main.s" && VERSION > 102
-        .db $04                 ; $04
+        .db $04                 ; @BT 04
         .else
         .db $05
         .endif
 
         .if "HELLO" != MONSTER && "1234" < "5678" && VERSION == VERSION || !1
-        .db $06                 ; $06
+        .db $06                 ; @BT 06
         .else
         .db $07
         .endif
@@ -297,39 +299,39 @@
         .if !("main.o" != FILE)
         .db $08
         .else
-        .db $09                 ; $09
+        .db $09                 ; @BT 09
         .endif
         
-        .db "<07"
+        .db "<07"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
         
-        .db "08>"
-        .db 1+1+1+1-1-1*2+1+1-1 ; $02
-        .db 1*2+3-4*2+2*3-1-1*2 ; $00
-        .db 1 & 1 + 2 * 3       ; $01
-        .db ((1 & 1) + 2) * 3   ; $09
-        .db 1 + <$1122 & 3      ; $03
-        .db 1 + >$1122 & 3      ; $02
-        .db "<08"
+        .db "08>"               ; @BT TEST-08 08 START
+        .db 1+1+1+1-1-1*2+1+1-1 ; @BT 02
+        .db 1*2+3-4*2+2*3-1-1*2 ; @BT 00
+        .db 1 & 1 + 2 * 3       ; @BT 01
+        .db ((1 & 1) + 2) * 3   ; @BT 09
+        .db 1 + <$1122 & 3      ; @BT 03
+        .db 1 + >$1122 & 3      ; @BT 02
+        .db "<08"               ; @BT END
         
         ////////////////////////////////////////////////////////////////////////
 
-        .db "09>"
+        .db "09>"               ; @BT TEST-09 09 START
 
         .if 1 > 0 && exists("main.s")
-          .db $00               ; $00
+          .db $00               ; @BT 00
           .if 1 < 0 || "hello" == "moto"
           .db $01
           .else
-          .db $02               ; $02
+          .db $02               ; @BT 02
           .endif
         .else
           .db $03
         .endif
 
         .if 1 > 0
-          .db $04               ; $04
+          .db $04               ; @BT 04
         .elif 2 > 1 && 3 != 2
           .db $05
         .endif
@@ -337,7 +339,7 @@
         .if 1 < 0
           .db $06
         .elif 2 > 1
-          .db $07               ; $07
+          .db $07               ; @BT 07
         .endif
 
         .if 1 < 0
@@ -345,7 +347,7 @@
         .elif 2 > 3
           .db $09
         .else
-          .db $0A               ; $0A
+          .db $0A               ; @BT 0A
         .endif
 
         .if 1 < 0
@@ -353,7 +355,7 @@
         .elif 2 > 3
           .db $0C
         .elif 4 > 2
-          .db $0D               ; $0D
+          .db $0D               ; @BT 0D
         .elif 5 > 1
           .db $0E
         .else
@@ -363,11 +365,11 @@
         .if 1 > 2
           .db $10
         .elif 2 > 1
-          .db $11               ; $11
+          .db $11               ; @BT 11
           .if 1 < 0
           .db $12
           .else
-          .db $13               ; $13
+          .db $13               ; @BT 13
           .endif
         .elif 3 > 2
           .db $14
@@ -375,14 +377,14 @@
           .db $15
         .endif
         
-        .db "<09"
+        .db "<09"               ; @BT END
 
         ////////////////////////////////////////////////////////////////////////
 
-        .db "10>"
+        .db "10>"               ; @BT TEST-10 10 START
 
         .if 2 > 1
-        .db $00                 ; $00
+        .db $00                 ; @BT 00
         .elif 4 > 1
         .db $01
         .elif 3 > 1
@@ -422,15 +424,15 @@
         .elif 3 < 1
         .db $12
         .elif 5 > 2
-          .db $13               ; $13
+          .db $13               ; @BT 13
           .if 1 < 0
           .db $14
           .elif 2 > 1
-            .db $15             ; $15
+            .db $15             ; @BT 15
             .if 0 != 0
             .db $20
             .elif 100 > 0
-            .db $21             ; $21
+            .db $21             ; @BT 21
             .elif 1 == 1
             .db $22
             .endif
@@ -456,11 +458,11 @@
         .db $1C
         .endif
         
-        .db "<10"
+        .db "<10"              ; @BT END
         
         ////////////////////////////////////////////////////////////////////////
 
-        .db "11>"
+        .db "11>"                 ; @BT TEST-11 11 START
 
         .if 0 == 1
         .db $00
@@ -468,7 +470,7 @@
           .if 1 == 0
           .db $01
           .elif 4 == 1+1+1+1+1+1-1-1
-          .db $02                 ; $02
+          .db $02                 ; @BT 02
           .else
             .if 1+1+1+1 == 6-1-1
             .db $03
@@ -488,7 +490,7 @@
           .elif 3 < 0
           .db $13
           .elif 1 > 1+1-1-1-1
-          .db $14               ; $14
+          .db $14               ; @BT 14
           .else
           .db $15
           .endif
@@ -504,4 +506,4 @@
           .db $19
           .endif
         .endif
-        .db "<11"
+        .db "<11"               ; @BT END
