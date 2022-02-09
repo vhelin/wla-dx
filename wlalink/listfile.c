004dabbfd47427a74265eddb43804692dbd08703
Ville Helin <ville.helin@gmail.com>
2022-02-10 00:55:18 +0200

    Removed an out-of-buffer read from listfile writer.

--- wlalink/listfile.c
+++ wlalink/listfile.c
@@ -283,7 +283,7 @@ int listfile_write_listfiles(struct section *sections) {
           while (m < file_size) {
             if (source_file[m] == 0xA) {
               m++;
-              if (source_file[m] == 0xD)
+              if (m < file_size && source_file[m] == 0xD)
                 m++;
               current_linenumber++;
               fprintf(f, "\n");
