3f206921ba4a4ee899f7447168c54226c92aa7c9
Ville Helin <ville.helin@gmail.com>
2023-05-20 17:33:55 +0300

    Added missing test file

---
+++ tests/gb-z80/namespace_test_2/fileA.asm
@@ -0,0 +1,8 @@
+
+        .struct "Instance"
+          name   ds 8
+          age    db
+          weight dw
+        .endst
+        
+        .dstruct Instance1 instanceof Instance DATA "jingle", 1, 2
