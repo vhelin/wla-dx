89a90a56be5c2b8cf19a9afa3e1b32384ddb1a97
Ville Helin <ville.helin@gmail.com>
2023-11-19 20:34:05 +0200

    Release v10.6

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ FILE *g_file_out_ptr = NULL;
 __near long __stack = 200000;
 #endif
 
-char s_version_string[] = "$VER: wla-" WLA_NAME " 10.6b (12.11.2023)";
+char s_version_string[] = "$VER: wla-" WLA_NAME " 10.6 (19.11.2023)";
 char s_wla_version[] = "10.6";
 
 extern struct incbin_file_data *g_incbin_file_data_first, *g_ifd_tmp;
@@ -1070,7 +1070,7 @@ int main(int argc, char *argv[]) {
   }
 
   if (g_output_format == OUTPUT_NONE || parse_flags_result == FAILED) {
-    char title[] = "WLA " ARCH_STR " Macro Assembler v10.6b";
+    char title[] = "WLA " ARCH_STR " Macro Assembler v10.6";
     int length, left, right;
 
     length = (int)strlen(title);
