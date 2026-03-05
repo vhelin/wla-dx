fef0509497dbf99572b39e9aebaf34309420eb2b
Ville Helin <ville.helin@gmail.com>
2026-03-06 00:58:07 +0200

    Added "-n <bytes>" to WLAB.

--- wlab/main.c
+++ wlab/main.c
@@ -14,11 +14,11 @@
 
 
 #if defined(AMIGA)
-char version_string[] = "$VER: WLAB 1.2 (31.03.2003)";
+char version_string[] = "$VER: WLAB 1.3 (6.3.2026)";
 #endif
 
 
-int g_header = ON, g_mode = MODE_NONE, g_print_address = OFF, g_address_size;
+int g_header = ON, g_mode = MODE_NONE, g_print_address = OFF, g_address_size, g_bytes_per_line = 8;
 
 
 int main(int argc, char *argv[]) {
@@ -26,27 +26,45 @@ int main(int argc, char *argv[]) {
   FILE *fp = NULL;
   unsigned char *in;
   char *name;
-  int i, file_size, t, address = 0, result;
+  int i, j, file_size, t, address = 0, result;
 
   result = SUCCEEDED;
 
-  if (!(argc == 2 || argc == 3))
+  if (argc < 3)
     result = FAILED;
 
-  if (argc == 3)
-    result = parse_flags(argv[1]);
+  for (i = 1; i < argc - 1 && result == SUCCEEDED; i++) {
+    if (strcmp(argv[i], "-n") == 0) {
+      i++;
+      if (i >= argc - 1) {
+        result = FAILED;
+        break;
+      }
+      {
+        int n = atoi(argv[i]);
+        if (n < 1) {
+          result = FAILED;
+          break;
+        }
+        g_bytes_per_line = n;
+      }
+    }
+    else {
+      result = parse_flags(argv[i]);
+    }
+  }
 
   if (g_mode == MODE_NONE)
     result = FAILED;
 
   if (result == FAILED) {
-    fprintf(stderr, "\nWLAB Binary to WLA DB Converter v1.2\n");
-    fprintf(stderr, "Written by Ville Helin 2000-2003\n");
-    fprintf(stderr, "USAGE: %s -[ap]{bdh} <BIN FILE>\n", argv[0]);
+    fprintf(stderr, "\nWLAB Binary to WLA DB Converter v1.3\n");
+    fprintf(stderr, "Written by Ville Helin 2000-2026\n");
+    fprintf(stderr, "USAGE: %s [-a] [-p] [-n <NUM>] {-b|-d|-h} <BIN FILE>\n", argv[0]);
     fprintf(stderr, "Commands:             Options:\n");
     fprintf(stderr, "b  Output binary      a  Print address\n");
     fprintf(stderr, "d  Output decimal     p  Skip header\n");
-    fprintf(stderr, "h  Output hex\n\n");
+    fprintf(stderr, "h  Output hex         n  Bytes per line (default: 8)\n\n");
     return 1;
   }
 
@@ -71,6 +89,8 @@ int main(int argc, char *argv[]) {
 
   if (fread(in, 1, file_size, fp) != (size_t) file_size) {
     fprintf(stderr, "Could not read all %d bytes of \"%s\"!", file_size, name);
+    free(in);
+    fclose(fp);
     return FAILED;
   }
 
@@ -109,14 +129,19 @@ int main(int argc, char *argv[]) {
 
     /* output hexadecimal data */
 
-    t = file_size >> 3;
-    file_size = file_size - (t << 3);
+    t = file_size / g_bytes_per_line;
+    file_size = file_size - t * g_bytes_per_line;
 
-    for (i = 0; t > 0; t--, i += 8) {
-      fprintf(stdout, ".DB $%.2x, $%.2x, $%.2x, $%.2x, $%.2x, $%.2x, $%.2x, $%.2x", in[i], in[i + 1], in[i + 2], in[i + 3], in[i + 4], in[i + 5], in[i + 6], in[i + 7]);
+    for (i = 0; t > 0; t--, i += g_bytes_per_line) {
+      fprintf(stdout, ".DB");
+      for (j = 0; j < g_bytes_per_line; j++) {
+        if (j > 0)
+          fprintf(stdout, ",");
+        fprintf(stdout, " $%.2x", in[i + j]);
+      }
       if (g_print_address == ON) {
-        print_address(address, address + 8-1);
-        address += 8;
+        print_address(address, address + g_bytes_per_line-1);
+        address += g_bytes_per_line;
       }
       else
         fprintf(stdout, "\n");
@@ -139,14 +164,19 @@ int main(int argc, char *argv[]) {
 
     /* output decimal data */
 
-    t = file_size >> 3;
-    file_size = file_size - (t << 3);
+    t = file_size / g_bytes_per_line;
+    file_size = file_size - t * g_bytes_per_line;
 
-    for (i = 0; t > 0; t--, i += 8) {
-      fprintf(stdout, ".DB %d, %d, %d, %d, %d, %d, %d, %d", in[i], in[i + 1], in[i + 2], in[i + 3], in[i + 4], in[i + 5], in[i + 6], in[i + 7]);
+    for (i = 0; t > 0; t--, i += g_bytes_per_line) {
+      fprintf(stdout, ".DB");
+      for (j = 0; j < g_bytes_per_line; j++) {
+        if (j > 0)
+          fprintf(stdout, ",");
+        fprintf(stdout, " %d", in[i + j]);
+      }
       if (g_print_address == ON) {
-        print_address(address, address + 8-1);
-        address += 8;
+        print_address(address, address + g_bytes_per_line-1);
+        address += g_bytes_per_line;
       }
       else
         fprintf(stdout, "\n");
@@ -166,26 +196,56 @@ int main(int argc, char *argv[]) {
     }
   }
   else if (g_mode == MODE_BIN) {
-    int b, c, d;
+    int b, c, d, k;
 
     /* output binary data */
 
-    for (i = 0; i < file_size; i++) {
-      fprintf(stdout, ".DB %%");
-      d = 0;
-      for (t = 7; t > -1; t--) {
-        b = 1 << t;
-        c = in[i] & b;
-        d = c >> t;
-        if (d == 0)
-          fprintf(stdout, "0");
-        else
-          fprintf(stdout, "1");
+    t = file_size / g_bytes_per_line;
+    file_size = file_size - t * g_bytes_per_line;
+
+    for (i = 0; t > 0; t--, i += g_bytes_per_line) {
+      fprintf(stdout, ".DB");
+      for (j = 0; j < g_bytes_per_line; j++) {
+        if (j > 0)
+          fprintf(stdout, ",");
+        fprintf(stdout, " %%");
+        for (k = 7; k > -1; k--) {
+          b = 1 << k;
+          c = in[i + j] & b;
+          d = c >> k;
+          if (d == 0)
+            fprintf(stdout, "0");
+          else
+            fprintf(stdout, "1");
+        }
       }
       if (g_print_address == ON) {
-        print_address(address, address);
-        address++;
+        print_address(address, address + g_bytes_per_line-1);
+        address += g_bytes_per_line;
       }
+      else
+        fprintf(stdout, "\n");
+    }
+
+    if (file_size != 0) {
+      fprintf(stdout, ".DB");
+      for (; file_size > 0; file_size--) {
+        fprintf(stdout, " %%");
+        for (k = 7; k > -1; k--) {
+          b = 1 << k;
+          c = in[i] & b;
+          d = c >> k;
+          if (d == 0)
+            fprintf(stdout, "0");
+          else
+            fprintf(stdout, "1");
+        }
+        i++;
+        if (file_size > 1)
+          fprintf(stdout, ",");
+      }
+      if (g_print_address == ON)
+        print_address(address, i-1);
       else
         fprintf(stdout, "\n");
     }
